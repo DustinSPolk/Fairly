5b9e818b7b83d1e5dae6bfcf4edfdfb1aa540bbd
DustinSPolk <122975992+DustinSPolk@users.noreply.github.com>
2023-05-10 11:45:18 -0400

    Update and rename Users_SelectAll_Paginated to Users_SelectAll_Paginated.sql

--- UserAdmin/SQL/Users_SelectAll_Paginated.sql
+++ UserAdmin/SQL/Users_SelectAll_Paginated.sql
@@ -18,18 +18,18 @@ GO
  --=============================================
  
  ALTER PROC [dbo].[Users_SelectAll_Paginated]
-				@PageIndex int
-				,@PageSize int
+		@PageIndex int
+		,@PageSize int
 AS
 
 /*---------------- TEST CODE -----------------
 
 	DECLARE		@PageIndex int = '0'
-				    ,@PageSize int = '10'
+			,@PageSize int = '10'
 
 	EXECUTE dbo.Users_SelectAll_Paginated
-				  @PageIndex
-				  ,@PageSize
+			@PageIndex
+			,@PageSize
 
 */
 
@@ -38,24 +38,24 @@ BEGIN
 	DECLARE @offset int = @PageIndex * @PageSize
 
 	SELECT	u.Id
-				  ,u.FirstName
-				  ,u.LastName
-				  ,u.Mi
-				  ,u.AvatarUrl
-				  ,u.Email
-				  ,u.IsConfirmed
-				  ,u.StatusId
-				  ,st.Name
-				  ,u.DateCreated
-				  ,u.DateModified
-				  ,u.IsProfileViewable
-				  ,TotalCount = COUNT(1) OVER()
+		,u.FirstName
+		,u.LastName
+		,u.Mi
+		,u.AvatarUrl
+		,u.Email
+		,u.IsConfirmed
+		,u.StatusId
+		,st.Name
+		,u.DateCreated
+		,u.DateModified
+		,u.IsProfileViewable
+		,TotalCount = COUNT(1) OVER()
 
-	FROM		[dbo].[Users] AS u
-				  INNER JOIN [dbo].[StatusTypes] AS st
-					  ON u.StatusId = st.Id
+	FROM	[dbo].[Users] AS u
+		INNER JOIN [dbo].[StatusTypes] AS st
+			ON u.StatusId = st.Id
 
-	ORDER BY	[Id]
+	ORDER BY [Id]
 
 	OFFSET @offset ROWS
 	FETCH NEXT @PageSize ROWS ONLY
