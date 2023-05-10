144cc76ee2c722e3104312e1c101d057b97975e0
DustinSPolk <122975992+DustinSPolk@users.noreply.github.com>
2023-05-10 11:39:25 -0400

    Update IGoogleAnalyticsReportService.cs

--- Analytics/.NET/IGoogleAnalyticsReportService.cs
+++ Analytics/.NET/IGoogleAnalyticsReportService.cs
@@ -1,10 +1,4 @@
-﻿using Google.Apis.AnalyticsReporting.v4.Data;
-using Sabio.Models.Requests.GoogleReportRequest;
-
-namespace Sabio.Services.Interfaces
-{
     public interface IGoogleAnalyticsReportService
     {
         GetReportsResponse GetAnalyticsReport(GoogleGetReportRequest model);
     }
-}
