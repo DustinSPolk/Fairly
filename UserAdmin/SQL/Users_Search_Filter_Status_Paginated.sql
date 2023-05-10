554447f748984f236490e12376604294a99074c2
DustinSPolk <122975992+DustinSPolk@users.noreply.github.com>
2023-05-10 11:43:43 -0400

    Update Users_Search_Filter_Status_Paginated.sql

--- UserAdmin/SQL/Users_Search_Filter_Status_Paginated.sql
+++ UserAdmin/SQL/Users_Search_Filter_Status_Paginated.sql
@@ -20,24 +20,24 @@ GO
  --=============================================
  
  ALTER PROC [dbo].[Users_Search_Filter_Status_Paginated]
-				@Query nvarchar(50)
-				,@StatusId int
-				,@PageIndex int
-				,@PageSize int
+		@Query nvarchar(50)
+		,@StatusId int
+		,@PageIndex int
+		,@PageSize int
 AS
 
 /*---------------- TEST CODE -----------------
 
 	DECLARE		@Query nvarchar(50) = 'Kolby'
-				,@StatusId int = '1'
-				,@PageIndex int = '0'
-				,@PageSize int = '10'
+			,@StatusId int = '1'
+			,@PageIndex int = '0'
+			,@PageSize int = '10'
 
 	EXECUTE [dbo].[Users_Search_Filter_Status_Paginated]
-				@Query
-				,@StatusId
-				,@PageIndex
-				,@PageSize
+			@Query
+			,@StatusId
+			,@PageIndex
+			,@PageSize
 
 */
 
@@ -45,29 +45,29 @@ BEGIN
 
 	DECLARE @offset int = @PageIndex * @PageSize
 
-	SELECT		u.Id
-				  ,u.FirstName
-				  ,u.LastName
-				  ,u.Mi
-				  ,u.AvatarUrl
-				  ,u.Email
-				  ,u.IsConfirmed
-				  ,u.StatusId
-				  ,st.Name
-				  ,u.DateCreated
-				  ,u.DateModified
-				  ,u.IsProfileViewable
-				  ,TotalCount = COUNT(1) OVER()
+	SELECT	u.Id
+		,u.FirstName
+		,u.LastName
+		,u.Mi
+		,u.AvatarUrl
+		,u.Email
+		,u.IsConfirmed
+		,u.StatusId
+		,st.Name
+		,u.DateCreated
+		,u.DateModified
+		,u.IsProfileViewable
+		,TotalCount = COUNT(1) OVER()
 
-	FROM		[dbo].[Users] AS u
-				  INNER JOIN [dbo].[StatusTypes] AS st
-					  ON u.StatusId = st.Id
+	FROM	[dbo].[Users] AS u
+		INNER JOIN [dbo].[StatusTypes] AS st
+			ON u.StatusId = st.Id
 
-	WHERE		(u.FirstName LIKE '%' + @Query + '%'
-				    OR u.LastName LIKE '%' + @Query + '%')
-				  AND u.StatusId = @StatusId
+	WHERE	(u.FirstName LIKE '%' + @Query + '%'
+		OR u.LastName LIKE '%' + @Query + '%')
+		AND u.StatusId = @StatusId
 
-	ORDER BY	[Id]
+	ORDER BY [Id]
 
 	OFFSET @offset ROWS
 	FETCH NEXT @PageSize ROWS ONLY
